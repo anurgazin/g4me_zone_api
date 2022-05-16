49201eb657b0a6d8b38bbf6efedcec15099d5497
anurgazin <54969694+anurgazin@users.noreply.github.com>
2022-05-16 14:28:50 -0400

    Create Article, Get All, Get By Id routes is created

--- routes_controllers/routes/article-route.js
+++ routes_controllers/routes/article-route.js
@@ -3,7 +3,7 @@ const express = require("express");
 const ArticleController = require("../controllers/article-ctrl");
 
 const router = express.Router();
-
+// routes
 router.post("/article", ArticleController.createArticle);
 router.get("/article/:id", ArticleController.getArticleById);
 router.get("/articles", ArticleController.getArticles);
