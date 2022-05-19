cab842defaf3535449a3b28047643f20de2cd704
anurgazin <54969694+anurgazin@users.noreply.github.com>
2022-05-18 23:15:17 -0400

    Articles upload

--- routes_controllers/controllers/article-ctrl.js
+++ routes_controllers/controllers/article-ctrl.js
@@ -1,48 +1,59 @@
 const Article = require("../../db/schemes/articleScheme");
-const multer = require("multer");
+// const multer = require("multer");
 
-const storage = multer.diskStorage({
-  destination: "./upload/",
-  filename: function (req, file, cb) {
-    console.log("Uploading Photo");
-    cb(null, Date.now() + file.originalname);
-  },
-});
 
-const upload = multer({ storage: storage });
+// const storage = multer.diskStorage({
+//   destination: "./upload/",
+//   filename: function (req, file, cb) {
+//     console.log("Uploading Photo");
+//     cb(null, Date.now() + file.originalname);
+//   },
+// });
 
-createArticle = upload.single("articleImage"),
-  (req, res) => {
-    //console.log(req.file);
-    const body = req.body;
-    console.log("I AM HERE");
-    if (!body) {
-      return res.status(400).json({
-        success: false,
-        error: "You must provide an article",
-      });
-    }
-    const article = new Article(body);
-    if (!article) {
-      return res.status(400).json({ success: false, error: err });
-    }
+// const upload = multer({ storage: storage });
 
-    article
-      .save()
-      .then(() => {
-        return res.status(201).json({
-          success: true,
-          id: article._id,
-          message: "Article created!",
-        });
-      })
-      .catch((error) => {
-        return res.status(400).json({
-          error,
-          message: "Article is not created!",
-        });
-      });
-  };
+// (createArticle = upload.single("articleImage")),
+//   (req, res) => {
+//     //console.log(req.file);
+//     let img = "";
+//     if (req.file) {
+//       img = req.file.path.replace("upload/", "");
+//     }
+//     const body = req.body;
+//     console.log("I AM HERE");
+//     if (!body) {
+//       return res.status(400).json({
+//         success: false,
+//         error: "You must provide an article",
+//       });
+//     }
+//     const article = new Article({
+//       title: body.title,
+//       text: body.text,
+//       rating: body.rating,
+//       image: img,
+//       date: Date.now(),
+//     });
+//     if (!article) {
+//       return res.status(400).json({ success: false, error: err });
+//     }
+
+//     article
+//       .save()
+//       .then(() => {
+//         return res.status(201).json({
+//           success: true,
+//           id: article._id,
+//           message: "Article created!",
+//         });
+//       })
+//       .catch((error) => {
+//         return res.status(400).json({
+//           error,
+//           message: "Article is not created!",
+//         });
+//       });
+//   };
 
 getArticleById = async (req, res) => {
   await Article.findOne({ _id: req.params.id })
@@ -75,7 +86,7 @@ getArticles = async (req, res) => {
 };
 
 module.exports = {
-  createArticle,
+  // createArticle,
   getArticleById,
   getArticles,
 };
